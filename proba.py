45a8ae4f7cc410a8f4fda8cbaa2507a8f6ca2c23
JJ <juhacar116@gmail.com>
2021-06-22 22:59:11 +0200

    Add files via upload

---
+++ proba.py
@@ -0,0 +1,38 @@
+import sys
+
+arr=[]
+i=0
+for line in sys.stdin:
+    arr.insert(i,line)
+    i=i+1
+
+#print("Ulazi "+ arr[0])
+#print("Pocetno "+ arr[4])
+#print(states[-1])
+
+print(arr)
+
+#ulaz = arr[0]
+#states = arr[5:]
+#q0=arr[4]
+
+#x=ulaz.split('|')
+
+
+
+#if thing in some_list: some_list.remove(thing)
+
+#i=0
+#for x in lista:
+#        if 'kraj' in lista:
+#            lista.remove('kraj')
+
+#        if i<2:
+#            i=i+1
+#            lista.append('kraj')
+
+
+#print(lista)
+
+
+
